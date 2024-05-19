376334e1bd2784d3aee9a69dc0918d270bf3247d
Azwan Triyadi <80760932+azwan20@users.noreply.github.com>
2024-05-19 21:39:36 +0800

    Update navbar.js

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -5,17 +5,17 @@ export default function Navbar() {
         <>
             <nav class="navbar navbar-expand-lg">
                 <div class="container-fluid d-flex justify-content-between">
-                    <a class="navbar-brand ps-5" href="#">TK Tutas Talenta Tanjung Tobaku</a>
+                    <a class="navbar-brand ps-5" href="#">TK Tunas Talenta Tanjung Tobaku</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse justify-content-end pe-5" id="navbarNav">
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="/form">Pendaftaran Siswa Baru</a>
+                                <Link class="nav-link active" aria-current="page" href="/form">Pendaftaran Siswa Baru</Link>
                             </li>
                             <li>
-                                <a class="nav-link active" aria-current="page" href="/dataSiswa">Edit Data</a>
+                                <Link class="nav-link active" aria-current="page" href="/dataSiswa">Edit Data</Link>
                             </li>
                             <li class="nav-item">
                                 <Link class="nav-link" href="/">Login</Link>
